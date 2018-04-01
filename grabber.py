7c8b53fa49ee0d94dbbdc40e74f05c03a4e818e6
Dorian Konrad <dorian.konrad@gmail.com>
2018-04-01 17:48:22 +0200

    Fix grabber to handle multiple acquisition sessions

--- grabber.py
+++ grabber.py
@@ -1,12 +1,18 @@
+#!/usr/bin/env python3
 import time
 import picamera
 from time import gmtime, strftime
+import uuid
+import os
+
+folder = uuid.uuid4().hex
+os.mkdir(folder)
 
 camera = picamera.PiCamera()
 camera.vflip = True
 camera.resolution = (3280//2, 2464//2)
 
 while(1):
-  name = strftime('images/%Y%m%d_%H_%M_%S.png', gmtime())
+  name = os.path.join(folder, strftime('%Y%m%d_%H_%M_%S.png', gmtime()))
   camera.capture(name, format='png')
   time.sleep(5)
