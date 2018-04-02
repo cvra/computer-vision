8be3f3e344d4fb65c1a136ad2d84a94e7cfa7b70
Dorian Konrad <dorian.konrad@gmail.com>
2018-04-02 15:26:36 +0200

    Pep8

--- planReader/setup.py
+++ planReader/setup.py
@@ -1,23 +1,21 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "PlanDetector",
-    version = "0.1",
-    author = "Dorian Konrad",
-    author_email = "dorian.konrad@gmail.com",
-    description = ("Construction Plan Reader for Eurobot 2018."),
-    license = "",
-    keywords = "",
-    url = "http://www.cvra.ch",
-    packages=['', ''],
+    name="PlanDetector",
+    version="0.1",
+    author="Dorian Konrad",
+    author_email="dorian.konrad@gmail.com",
+    description=("Construction Plan Reader for Eurobot 2018."),
+    license="",
+    keywords="",
+    url="http://www.cvra.ch",
+    packages=find_packages(),
     long_description=read('README'),
     classifiers=[],
 )
