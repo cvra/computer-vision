ddf15d42acb1fe8d66f124fb6e2ca2d542e132fd
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-04-07 20:41:48 +0200

    Exit gracefully on ctrl-C

--- grab_and_detect.py
+++ grab_and_detect.py
@@ -1,3 +1,4 @@
+import sys
 from time import gmtime, strftime
 
 import picamera
@@ -17,14 +18,18 @@ camera.resolution = (3280//2, 2464//2)
 print('{}: Start'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
 
 while(True):
-    stream = picamera.array.PiRGBArray(camera)
-    print('{}: Grab Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
-    camera.capture(stream, format='rgb', use_video_port=True)
+    try:
+        stream = picamera.array.PiRGBArray(camera)
+        print('{}: Grab Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+        camera.capture(stream, format='rgb', use_video_port=True)
 
-    print('{}: Load Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
-    im = stream.array
+        print('{}: Load Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+        im = stream.array
 
-    print('{}: Process Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
-    color_plan = plan.process(im)
+        print('{}: Process Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+        color_plan = plan.process(im)
 
-    print('{}: Output {}'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime()), color_plan))
+        print('{}: Output {}'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime()), color_plan))
+    except KeyboardInterrupt:
+        print('Exiting program after Ctrl-C')
+        sys.exit()
