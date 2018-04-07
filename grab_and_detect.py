43a655a258131a0686741948df3ce271191408b1
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-04-07 20:39:41 +0200

    Don't use with/as when variable assigment is fine

--- grab_and_detect.py
+++ grab_and_detect.py
@@ -9,27 +9,22 @@ from planReader.planReader import planReader
 plan = planReader(config_path='conf.yaml',
                   config_color='colors.yaml', debug=False)
 
-with picamera.PiCamera() as camera:
-    camera.vflip = True
-    camera.hflip = True
-    camera.resolution = (3280//2, 2464//2)
+camera = picamera.PiCamera()
+camera.vflip = True
+camera.hflip = True
+camera.resolution = (3280//2, 2464//2)
 
-    print('{}: Start'.format(strftime("%a, %d %b %Y %H:%M:%S",
-                                      gmtime())))
+print('{}: Start'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
 
-    while(True):
-        with picamera.array.PiRGBArray(camera) as stream:
-            print('{}: Grab Image'.format(
-                strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
-            camera.capture(stream, format='rgb', use_video_port=True)
+while(True):
+    stream = picamera.array.PiRGBArray(camera)
+    print('{}: Grab Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+    camera.capture(stream, format='rgb', use_video_port=True)
 
-            print('{}: Load Image'.format(
-                strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
-            im = stream.array
+    print('{}: Load Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+    im = stream.array
 
-            print('{}: Process Image'.format(
-                strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
-            color_plan = plan.process(im)
+    print('{}: Process Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+    color_plan = plan.process(im)
 
-            print('{}: Output {}'.format(
-                strftime("%a, %d %b %Y %H:%M:%S", gmtime()), color_plan))
+    print('{}: Output {}'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime()), color_plan))
