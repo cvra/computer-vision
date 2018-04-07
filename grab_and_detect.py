547ae10ef2c13bcc78b4edb07b0c3a988da14dec
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-04-07 21:01:19 +0200

    Move execution into a main function

--- grab_and_detect.py
+++ grab_and_detect.py
@@ -16,43 +16,47 @@ VALID_COLORS = {'Black':'K', 'Yellow':'Y', 'Orange':'O', 'Blue':'B', 'Green':'G'
 
 logging.basicConfig(level=logging.INFO)
 
-plan = planReader(config_path='conf.yaml',
-                  config_color='colors.yaml', debug=False)
+def main():
+    plan = planReader(config_path='conf.yaml',
+                      config_color='colors.yaml', debug=False)
 
-ser = serial.Serial('/dev/serial0', 19200)
-logging.info('Serial port {} ready for streaming results'.format(ser.name))
+    ser = serial.Serial('/dev/serial0', 19200)
+    logging.info('Serial port {} ready for streaming results'.format(ser.name))
 
-camera = picamera.PiCamera()
-camera.vflip = True
-camera.hflip = True
-camera.resolution = (3280//2, 2464//2)
+    camera = picamera.PiCamera()
+    camera.vflip = True
+    camera.hflip = True
+    camera.resolution = (3280//2, 2464//2)
 
-logging.info('{}: Start'.format(time_str()))
+    logging.info('{}: Start'.format(time_str()))
 
-while(True):
-    try:
-        stream = picamera.array.PiRGBArray(camera)
-        logging.info('{}: Grab Image'.format(time_str()))
-        camera.capture(stream, format='rgb', use_video_port=True)
+    while(True):
+        try:
+            stream = picamera.array.PiRGBArray(camera)
+            logging.info('{}: Grab Image'.format(time_str()))
+            camera.capture(stream, format='rgb', use_video_port=True)
 
-        logging.info('{}: Load Image'.format(time_str()))
-        im = stream.array
+            logging.info('{}: Load Image'.format(time_str()))
+            im = stream.array
 
-        logging.info('{}: Process Image'.format(time_str()))
-        color_plan = plan.process(im)
+            logging.info('{}: Process Image'.format(time_str()))
+            color_plan = plan.process(im)
 
-        logging.info('{}: Output {}'.format(time_str(), color_plan))
+            logging.info('{}: Output {}'.format(time_str(), color_plan))
 
-        if len(color_plan) == 3:
-            sequences, distance = error_correction(color_plan)
-            logging.info('{}: Closest valid sequence {} that was {} color away from prediction'.format(time_str(), sequences[0], distance))
+            if len(color_plan) == 3:
+                sequences, distance = error_correction(color_plan)
+                logging.info('{}: Closest valid sequence {} that was {} color away from prediction'.format(time_str(), sequences[0], distance))
 
-            compact_res = ''.join(VALID_COLORS[color] for color in sequences[0]) + '\n'
-            ser.write(compact_res.encode())
-            logging.info('UART stream: {}'.format(compact_res))
-        else:
-            logging.warning('Invalid color sequence detected {} does not contain exactly 3 colors'.format(color_plan))
-    except KeyboardInterrupt:
-        logging.info('Exiting program after Ctrl-C')
-        ser.close()
-        sys.exit()
+                compact_res = ''.join(VALID_COLORS[color] for color in sequences[0]) + '\n'
+                ser.write(compact_res.encode())
+                logging.info('UART stream: {}'.format(compact_res))
+            else:
+                logging.warning('Invalid color sequence detected {} does not contain exactly 3 colors'.format(color_plan))
+        except KeyboardInterrupt:
+            logging.info('Exiting program after Ctrl-C')
+            ser.close()
+            sys.exit()
+
+if __name__ == '__main__':
+    main()
