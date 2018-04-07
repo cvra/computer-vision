afa5d41eccb94b3590a4bc39b6d31366815ee8d5
Dorian Konrad <dorian.konrad@gmail.com>
2018-04-07 12:30:36 +0200

    Read the camera image as a stream and load it into a buffer. This saves a lot of time comparing to the previous method that save and read the image on a file.

--- grab_and_detect.py
+++ grab_and_detect.py
@@ -1,27 +1,35 @@
-import cv2
 import time
 import picamera
+import picamera.array
 
 from time import gmtime, strftime
 from planReader.planReader import planReader
 
-camera = picamera.PiCamera()
-camera.vflip = True
-camera.hflip = True
-camera.resolution = (3280//2, 2464//2)
 
 plan = planReader(config_path='conf.yaml',
                   config_color='colors.yaml', debug=False)
 
-while(1):
-    print('{}: Start'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
-    print('{}: Grab Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
-    name = strftime('out.png')
-    camera.capture(name, format='png')
+with picamera.PiCamera() as camera:
+    camera.vflip = True
+    camera.hflip = True
+    camera.resolution = (3280//2, 2464//2)
 
-    print('{}: Load Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
-    im = cv2.imread(name, cv2.IMREAD_COLOR)
-    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-    print('{}: Process Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
-    res = plan.process(im)
-    print('{}: Output {}'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime()), res))
+    print('{}: Start'.format(strftime("%a, %d %b %Y %H:%M:%S",
+                                      gmtime())))
+
+    while(True):
+        with picamera.array.PiRGBArray(camera) as stream:
+            print('{}: Grab Image'.format(
+                strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+            camera.capture(stream, format='rgb', use_video_port=True)
+
+            print('{}: Load Image'.format(
+                strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+            im = stream.array
+
+            print('{}: Process Image'.format(
+                strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+            color_plan = plan.process(im)
+
+            print('{}: Output {}'.format(
+                strftime("%a, %d %b %Y %H:%M:%S", gmtime()), color_plan))
