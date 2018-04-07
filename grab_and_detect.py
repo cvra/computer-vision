015bd21ad50c9c7f25292000488950e9edb95d77
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-04-07 21:00:10 +0200

    Factor out time string in helper

--- grab_and_detect.py
+++ grab_and_detect.py
@@ -9,6 +9,9 @@ import serial
 from error_correction import error_correction
 from planReader.planReader import planReader
 
+def time_str():
+    return strftime("%a, %d %b %Y %H:%M:%S", gmtime())
+
 VALID_COLORS = {'Black':'K', 'Yellow':'Y', 'Orange':'O', 'Blue':'B', 'Green':'G'}
 
 logging.basicConfig(level=logging.INFO)
@@ -24,25 +27,25 @@ camera.vflip = True
 camera.hflip = True
 camera.resolution = (3280//2, 2464//2)
 
-logging.info('{}: Start'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+logging.info('{}: Start'.format(time_str()))
 
 while(True):
     try:
         stream = picamera.array.PiRGBArray(camera)
-        logging.info('{}: Grab Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+        logging.info('{}: Grab Image'.format(time_str()))
         camera.capture(stream, format='rgb', use_video_port=True)
 
-        logging.info('{}: Load Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+        logging.info('{}: Load Image'.format(time_str()))
         im = stream.array
 
-        logging.info('{}: Process Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+        logging.info('{}: Process Image'.format(time_str()))
         color_plan = plan.process(im)
 
-        logging.info('{}: Output {}'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime()), color_plan))
+        logging.info('{}: Output {}'.format(time_str(), color_plan))
 
         if len(color_plan) == 3:
             sequences, distance = error_correction(color_plan)
-            logging.info('{}: Closest valid sequence {} that was {} color away from prediction'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime()), sequences[0], distance))
+            logging.info('{}: Closest valid sequence {} that was {} color away from prediction'.format(time_str(), sequences[0], distance))
 
             compact_res = ''.join(VALID_COLORS[color] for color in sequences[0]) + '\n'
             ser.write(compact_res.encode())
