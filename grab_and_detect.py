3dec0e82a78c3ce964a8d89e4b4a77c66dd5885f
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-04-07 20:42:35 +0200

    Use logging instead of print

--- grab_and_detect.py
+++ grab_and_detect.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 from time import gmtime, strftime
 
@@ -15,21 +16,21 @@ camera.vflip = True
 camera.hflip = True
 camera.resolution = (3280//2, 2464//2)
 
-print('{}: Start'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+logging.info('{}: Start'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
 
 while(True):
     try:
         stream = picamera.array.PiRGBArray(camera)
-        print('{}: Grab Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+        logging.info('{}: Grab Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
         camera.capture(stream, format='rgb', use_video_port=True)
 
-        print('{}: Load Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+        logging.info('{}: Load Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
         im = stream.array
 
-        print('{}: Process Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+        logging.info('{}: Process Image'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
         color_plan = plan.process(im)
 
-        print('{}: Output {}'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime()), color_plan))
+        logging.info('{}: Output {}'.format(strftime("%a, %d %b %Y %H:%M:%S", gmtime()), color_plan))
     except KeyboardInterrupt:
-        print('Exiting program after Ctrl-C')
+        logging.info('Exiting program after Ctrl-C')
         sys.exit()
